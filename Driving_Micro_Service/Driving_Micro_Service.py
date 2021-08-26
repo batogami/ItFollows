5b7fadda77679ef15d09dfeb8515a511cb76777a
mb-ecolytiq <maximilian.bartels@ecolytiq.com>
2021-08-26 21:36:18 +0200

    Adding first server components, Flask is used

---
+++ Driving_Micro_Service/Driving_Micro_Service.py
@@ -0,0 +1,12 @@
+from flask import Flask
+from flask_restful import Resource, Api
+
+app = Flask(__name__)
+api = Api(app)
+
+class DrivingMicroService():
+    #TODO:Write REST Service
+    a = 1
+
+if __name__ == '__main__':
+    app.run(debug=True)
